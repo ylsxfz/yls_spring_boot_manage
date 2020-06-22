dccd4a3c2f29424e9a1ac637e4dad9fc13ac9fdf
yls <2689623200@qq.com>
2020-06-22 14:52:05 +0800

    spring-boot 项目监控

---
+++ src/main/java/com/spring/yls_spring_boot_manage/YlsSpringBootManageApplication.java
@@ -0,0 +1,63 @@
+package com.spring.yls_spring_boot_manage;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@SpringBootApplication
+@EnableAsync
+@EnableAdminServer
+@EnableDiscoveryClient
+public class YlsSpringBootManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlsSpringBootManageApplication.class, args);
+    }
+
+    /**
+     * @Author yls
+     * @Description  安全认证的相关配置
+     * @Date 2020/3/30 22:38
+     **/
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+            successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringAntMatchers(
+                            adminContextPath + "/instances",
+                            adminContextPath + "/actuator/**"
+                    );
+
+            //允许页面被嵌套加载
+            http.headers().frameOptions().disable();
+        }
+    }
+}
---
+++ src/test/java/com/spring/yls_spring_boot_manage/YlsSpringBootManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.yls_spring_boot_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlsSpringBootManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
